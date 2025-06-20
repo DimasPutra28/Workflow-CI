4bf1b77b3b9098ca92c0dec2d6e2d609f1a91e37
DimasPutra28 <dimas.meidiansyah.putra-2021@vokasi.unair.ac.id>
2025-06-20 07:53:30 +0700

    progress

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,44 +3,48 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
-import sys
-import os
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,
+    roc_auc_score, log_loss, matthews_corrcoef, balanced_accuracy_score
+)
+import argparse
 
-if __name__ == "__main__":
+# Ambil data_path dari argumen
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', type=str, default='Student_performance_processed_data.csv')
+args = parser.parse_args()
 
-    np.random.seed(42)
+data = pd.read_csv(args.data_path)
 
-    # get file path from arguments
-    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "Student_performance_processed_data.csv")
-    data = pd.read_csv(file_path)
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        data.drop("GradeClass", axis=1),
-        data["GradeClass"],
-        random_state=42,
-        test_size=0.2
-    )
+mlflow.set_experiment("Student Performance")
 
-    input_example = X_train[0:5]
-    dataset = mlflow.data.from_pandas(data)
+X_train, X_test, y_train, y_test = train_test_split(
+    data.drop("GradeClass", axis=1),
+    data["GradeClass"],
+    random_state=42,
+    test_size=0.2
+)
 
-    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+input_example = X_train[0:5]
+dataset = mlflow.data.from_pandas(data)
 
-    with mlflow.start_run():
+with mlflow.start_run():
 
-        mlflow.autolog()
+    mlflow.autolog()
 
-        model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-        model.fit(X_train, y_train)
+    model.fit(X_train, y_train)
 
-        mlflow.sklearn.log_model(
-                sk_model=model,
-                name="model",
-                input_example=input_example
-            )
+    mlflow.sklearn.log_model(
+            sk_model=model,
+            name="model",
+            input_example=input_example
+        )
 
-        accuracy = model.score(X_test, y_test)
+    accuracy = model.score(X_test, y_test)
 
-        # Log dataset
-        mlflow.log_input(dataset, context="training")
+    # Log dataset
+    mlflow.log_input(dataset, context="training")

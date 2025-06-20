4b9f5005138da28d74b646808195ea33d142ede4
DimasPutra28 <dimas.meidiansyah.putra-2021@vokasi.unair.ac.id>
2025-06-20 11:10:14 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -12,7 +12,7 @@ import joblib
 import os
 
 # Set MLflow tracking
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 mlflow.set_experiment("Student Performance CI")
 
 # Load data
